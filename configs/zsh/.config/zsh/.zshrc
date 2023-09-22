f74d724e6b16129ca069c9d1138926dc0b727dba
Pedro Ferreira <pedrodsf21@gmail.com>
2023-09-22 17:48:45 +0100

    Fix XDG_CONFIG_HOME source at startup

--- configs/zsh/.config/zsh/.zshrc
+++ configs/zsh/.config/zsh/.zshrc
@@ -1,9 +1,10 @@
 # ------------------------------
+# PLUGINS
+# ------------------------------
 plugins=(
   jeffreytse/zsh-vi-mode
   zsh-users/zsh-completions
   zsh-users/zsh-autosuggestions
-  zsh-users/zsh-history-substring-search
   hlissner/zsh-autopair
   # must be last
   zdharma/fast-syntax-highlighting
@@ -12,11 +13,28 @@ plugins=(
 source $ZDOTDIR/plugins.zsh $plugins
 
 unset plugins
+
+# ------------------------------
+# HISTORY
+# ------------------------------
+export HISTFILE=~/.history
+export HISTSIZE=1000000   # the number of items for the internal history list
+export SAVEHIST=1000000   # maximum number of items for the history file
+
+# The meaning of these options can be found in man page of `zshoptions`.
+setopt HIST_IGNORE_ALL_DUPS  # do not put duplicated command into history list
+setopt HIST_SAVE_NO_DUPS  # do not save duplicated command
+setopt HIST_REDUCE_BLANKS  # remove unnecessary blanks
+setopt INC_APPEND_HISTORY_TIME  # append command to history file immediately after execution
+setopt EXTENDED_HISTORY  # record command start time
 # ------------------------------
 
 export MANPAGER="nvim +Man!"
+export BAT_THEME="Monokai Extended Origin"
 
+# ------------------------------
 # ALIASES
+# ------------------------------
 alias vs="code"
 alias vim="nvim"
 alias configs="cd ~/.dotfiles/configs/"
--- configs/zsh/.config/zsh/plugins.zsh
+++ configs/zsh/.config/zsh/plugins.zsh
@@ -10,10 +10,6 @@ github_plugins=("$@")
 for plugin in "${plugins[@]}"; do
     plugin_name=${plugin#*/}
 
-    # Custom config for zsh-vi-mode
-    if [[ "$plugin_name" == "zsh-vi-mode" ]]; then
-        eval "function zvm_config() { ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT; ZVM_VI_INSERT_ESCAPE_BINDKEY=jk; }"
-    fi
     # Clone the plugin from GitHub if it doesn't exist
     if [[ ! -d "${ZDOTDIR:-$HOME}/plugins/$plugin_name" ]]; then
         mkdir -p "${ZDOTDIR:-$HOME}/plugins/${plugin_name}"
@@ -21,13 +17,27 @@ for plugin in "${plugins[@]}"; do
         git clone --depth 1 --recursive "https://github.com/$plugin.git" "${ZDOTDIR:-$HOME}/plugins/$plugin_name" 2>/dev/null
     fi
 
-  # Load the plugin
-  for initscript in "${plugin_name}.zsh" "${plugin_name}.plugin.zsh" "${plugin_name}.sh"; do
-      if [ -f "${ZDOTDIR:-$HOME}/plugins/$plugin_name/$initscript" ]; then
-          source "${ZDOTDIR:-$HOME}/plugins/$plugin_name/$initscript"
-          break
-      fi
-  done
+    # Custom config for zsh-vi-mode
+    if [[ "$plugin_name" == "zsh-vi-mode" ]]; then
+        eval "function zvm_config() { ZVM_LINE_INIT_MODE=$ZVM_MODE_INSERT; ZVM_VI_INSERT_ESCAPE_BINDKEY=jk; }"
+    elif [[ "$plugin_name" == "zsh-completions" ]]; then
+        fpath=("${ZDOTDIR:-$HOME}/plugins/$plugin_name/src" $fpath)
+        autoload -Uz compinit
+        compinit -u
+    fi
+
+    # Load the plugin
+    for initscript in "${plugin_name}.zsh" "${plugin_name}.plugin.zsh" "${plugin_name}.sh"; do
+        if [ -f "${ZDOTDIR:-$HOME}/plugins/$plugin_name/$initscript" ]; then
+            source "${ZDOTDIR:-$HOME}/plugins/$plugin_name/$initscript"
+            break
+        fi
+    done
 
 done
+
+unset plugin
+unset plugin_name
+unset github_plugins
+unset initscript
 
