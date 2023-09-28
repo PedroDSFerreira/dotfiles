dde2fb84b2545fb5227f3900a55c1a6f5d4694bf
Pedro Ferreira <pedrodsf21@gmail.com>
2023-09-28 11:46:26 +0100

    Update gitsigns mappings

--- configs/nvim/.config/nvim/lua/pedro/plugins/gitsigns.lua
+++ configs/nvim/.config/nvim/lua/pedro/plugins/gitsigns.lua
@@ -1,14 +1,21 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    signs = {
-      add = { text = "│" },
-      change = { text = "│" },
-      delete = { text = "󰍵" },
-      topdelete = { text = "‾" },
-      changedelete = { text = "~" },
-      untracked = { text = "│" },
-    },
-  }
+	"lewis6991/gitsigns.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		signs = {
+			add = { text = "│" },
+			change = { text = "│" },
+			delete = { text = "󰍵" },
+			topdelete = { text = "‾" },
+			changedelete = { text = "~" },
+			untracked = { text = "│" },
+		},
+	},
+	init = function()
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
+
+		keymap.set("n", "<leader>gp", "<cmd>:Gitsigns preview_hunk <CR>", { desc = "Preview hunk" })
+		keymap.set("n", "<leader>gl", "<cmd>:Gitsigns toggle_current_line_blame <CR>", { desc = "Line blame" })
+	end,
 }
