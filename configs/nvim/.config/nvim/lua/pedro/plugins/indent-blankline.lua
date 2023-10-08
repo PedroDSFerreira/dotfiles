b62c251d63b16b0c1a59a5166ad200c9c285da8a
Pedro Ferreira <pedrodsf21@gmail.com>
2023-10-08 11:40:53 +0100

    Update indent-blankline config

--- configs/nvim/.config/nvim/lua/pedro/plugins/indent-blankline.lua
+++ configs/nvim/.config/nvim/lua/pedro/plugins/indent-blankline.lua
@@ -1,5 +1,16 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	main = "ibl",
-	config = true,
+	-- config = true,
+	opts = {
+		indent = {
+
+			char = "│",
+		},
+		-- context_char = "│",
+		scope = {
+			show_start = false,
+			show_end = false,
+		},
+	},
 }
