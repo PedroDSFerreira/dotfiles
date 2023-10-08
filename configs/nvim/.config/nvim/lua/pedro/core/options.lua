a394e5d22ab7ac3bc1f6ac1d81360241ed602853
Pedro Ferreira <pedrodsf21@gmail.com>
2023-10-08 11:19:16 +0100

    Update tab options

--- configs/nvim/.config/nvim/lua/pedro/core/options.lua
+++ configs/nvim/.config/nvim/lua/pedro/core/options.lua
@@ -1,11 +1,12 @@
 local opt = vim.opt
 
+opt.mouse = nil
 opt.nu = true
 opt.relativenumber = true
 
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
+-- opt.tabstop = 4
+-- opt.softtabstop = 4
+-- opt.shiftwidth = 4
 opt.expandtab = true
 opt.smartindent = true
 
