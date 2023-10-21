e2713ec82c03b58a47778105548126f4ba380504
Pedro Ferreira <pedrodsf21@gmail.com>
2023-10-21 17:21:54 +0100

    Update icons

--- configs/polybar/.scripts/player-mpris-tail.py
+++ configs/polybar/.scripts/player-mpris-tail.py
@@ -662,7 +662,7 @@ parser.add_argument(
 parser.add_argument("--truncate-text", default="…")
 parser.add_argument("--icon-playing", default="")
 parser.add_argument("--icon-paused", default="")
-parser.add_argument("--icon-stopped", default="")
+parser.add_argument("--icon-stopped", default=" ")
 parser.add_argument("--icon-none", default="")
 args = parser.parse_args()
 
