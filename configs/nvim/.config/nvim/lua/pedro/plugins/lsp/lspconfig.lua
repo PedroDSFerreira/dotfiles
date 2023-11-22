3243b853ef4510f2d47f4b612c6f973b06c36a81
Pedro Ferreira <pedrodsf21@gmail.com>
2023-11-22 18:21:50 +0000

    Fix diagnostics popups

--- configs/nvim/.config/nvim/lua/pedro/plugins/lsp/lspconfig.lua
+++ configs/nvim/.config/nvim/lua/pedro/plugins/lsp/lspconfig.lua
@@ -14,57 +14,83 @@ return {
 
 		local keymap = vim.keymap -- for conciseness
 
-		local opts = { noremap = true, silent = true }
+		local key_opts = { noremap = true, silent = true }
 		local on_attach = function(_, bufnr)
-			opts.buffer = bufnr
+			key_opts.buffer = bufnr
 
 			-- set keybinds
-			opts.desc = "Code references"
-			keymap.set("n", "<leader>cR", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+			key_opts.desc = "Code references"
+			keymap.set("n", "<leader>cR", "<cmd>Telescope lsp_references<CR>", key_opts) -- show definition, references
 
-			opts.desc = "Code declaration"
-			keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, opts) -- go to declaration
+			key_opts.desc = "Code declaration"
+			keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, key_opts) -- go to declaration
 
-			opts.desc = "Code definitions"
-			keymap.set("n", "<leader>cd", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+			key_opts.desc = "Code definitions"
+			keymap.set("n", "<leader>cd", "<cmd>Telescope lsp_definitions<CR>", key_opts) -- show lsp definitions
 
-			opts.desc = "Code implementations"
-			keymap.set("n", "<leader>ci", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+			key_opts.desc = "Code implementations"
+			keymap.set("n", "<leader>ci", "<cmd>Telescope lsp_implementations<CR>", key_opts) -- show lsp implementations
 
-			opts.desc = "Code type definitions"
-			keymap.set("n", "<leader>ct", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+			key_opts.desc = "Code type definitions"
+			keymap.set("n", "<leader>ct", "<cmd>Telescope lsp_type_definitions<CR>", key_opts) -- show lsp type definitions
 
-			opts.desc = "Code actions"
-			keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+			key_opts.desc = "Code actions"
+			keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, key_opts) -- see available code actions, in visual mode will apply to selection
 
-			opts.desc = "Smart rename"
-			keymap.set("n", "<leader>cr", vim.lsp.buf.rename, opts) -- smart rename
+			key_opts.desc = "Smart rename"
+			keymap.set("n", "<leader>cr", vim.lsp.buf.rename, key_opts) -- smart rename
 
-			opts.desc = "Show buffer diagnostics"
-			keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+			key_opts.desc = "Show buffer diagnostics"
+			keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", key_opts) -- show  diagnostics for file
 
-			opts.desc = "Show line diagnostics"
-			keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
+			key_opts.desc = "Show line diagnostics"
+			keymap.set("n", "<leader>d", vim.diagnostic.open_float, key_opts) -- show diagnostics for line
 
-			opts.desc = "Go to previous diagnostic"
-			keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+			key_opts.desc = "Go to previous diagnostic"
+			keymap.set("n", "[d", vim.diagnostic.goto_prev, key_opts) -- jump to previous diagnostic in buffer
 
-			opts.desc = "Go to next diagnostic"
-			keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+			key_opts.desc = "Go to next diagnostic"
+			keymap.set("n", "]d", vim.diagnostic.goto_next, key_opts) -- jump to next diagnostic in buffer
 
-			opts.desc = "Show documentation for what is under cursor"
-			keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+			key_opts.desc = "Show documentation for what is under cursor"
+			keymap.set("n", "K", vim.lsp.buf.hover, key_opts) -- show documentation for what is under cursor
 		end
 
 		-- used to enable autocompletion (assign to every lsp server config)
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- -- Change the Diagnostic symbols in the sign column (gutter)
-		-- -- (not in youtube nvim video)
+		-------------------- DIAGNOSTICS
 		local signs = { Error = " ", Warn = " ", Hint = "󰛩 ", Info = " " }
 		for type, icon in pairs(signs) do
 			local hl = "DiagnosticSign" .. type
 			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
+
+		vim.diagnostic.config({
+			virtual_text = {
+				prefix = "●", -- Could be '■', '▎', 'x'
+			},
+		})
+
+		vim.cmd([[autocmd! ColorScheme * highlight NormalFloat guibg=#ffffff]])
+		vim.cmd([[autocmd! ColorScheme * highlight FloatBorder guifg=white guibg=#ffffff]])
+
+		local border = {
+			{ "╭", "FloatBorder" },
+			{ "─", "FloatBorder" },
+			{ "╮", "FloatBorder" },
+			{ "│", "FloatBorder" },
+			{ "╯", "FloatBorder" },
+			{ "─", "FloatBorder" },
+			{ "╰", "FloatBorder" },
+			{ "│", "FloatBorder" },
+		}
+
+		local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+		function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+			opts = opts or {}
+			opts.border = opts.border or border
+			return orig_util_open_floating_preview(contents, syntax, opts, ...)
 		end
 
 		--------------------
