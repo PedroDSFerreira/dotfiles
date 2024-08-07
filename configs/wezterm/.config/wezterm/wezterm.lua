9fb976cf3a008082ed986870720a62af5f7619aa
Pedro Ferreira <pedrodsf21@gmail.com>
2024-08-07 11:58:29 +0100

    Add transparent bakground

--- configs/wezterm/.config/wezterm/wezterm.lua
+++ configs/wezterm/.config/wezterm/wezterm.lua
@@ -1,17 +1,24 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
 
+config.background = {
+  {
+    source = {
+      Color = '#000000'
+    },
+    width = "100%",
+    height = "100%",
+    opacity = 0.70
+  },
+}
 config.font_size = 16.0
 config.color_scheme = 'Catppuccin Mocha'
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.3
 config.macos_window_background_blur = 20
 config.enable_tab_bar = false
 config.window_decorations = "NONE"
 config.disable_default_key_bindings = true
-
 
 return config
