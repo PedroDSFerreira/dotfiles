f4708f1920f4517ffc445a40c047731a039b9281
Pedro Ferreira <pedrodsf21@gmail.com>
2024-08-15 17:07:54 +0100

    Update wezterm config

--- configs/wezterm/.config/wezterm/wezterm.lua
+++ configs/wezterm/.config/wezterm/wezterm.lua
@@ -3,22 +3,23 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 
-config.background = {
-  {
-    source = {
-      Color = '#000000'
-    },
-    width = "100%",
-    height = "100%",
-    opacity = 0.70
-  },
-}
-config.font_size = 16.0
+-- config.background = {
+--   {
+--     source = {
+--       Color = '#000000'
+--     },
+--     width = "100%",
+--     height = "100%",
+--     -- opacity = 0.30
+--   },
+-- }
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = 'Medium' })
+config.font_size = 18.0
 config.color_scheme = 'Catppuccin Mocha'
-config.window_background_opacity = 0.3
+-- config.window_background_opacity = 0.7
 config.macos_window_background_blur = 20
 config.enable_tab_bar = false
-config.window_decorations = "NONE"
+config.window_decorations = "RESIZE"
 config.disable_default_key_bindings = true
 
 return config
