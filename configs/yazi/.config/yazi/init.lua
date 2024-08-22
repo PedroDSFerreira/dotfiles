61e9917cca63b57a43e9f5f61f4f2691eff0985b
Pedro Ferreira <pedrodsf21@gmail.com>
2024-08-22 22:53:46 +0100

    Lint init.lua

--- configs/yazi/.config/yazi/init.lua
+++ configs/yazi/.config/yazi/init.lua
@@ -1,7 +1,5 @@
--- ~/.config/yazi/init.lua
-require("relative-motions"):setup({ show_numbers="relative", show_motion = true })
-require("git"):setup{
-}
-require("session"):setup {
-	sync_yanked = true,
-}
+require("relative-motions"):setup({ show_numbers = "relative", show_motion = true })
+require("git"):setup({})
+require("session"):setup({
+    sync_yanked = true,
+})
