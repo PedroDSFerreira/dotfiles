ab1cdc046c65176cb50a036ba7628cd332ac5ae9
Pedro Ferreira <pedrodsf21@gmail.com>
2024-09-12 17:27:48 +0100

    Fix better-escape visual selection

--- configs/nvim/.config/nvim/lua/pedro/plugins/better-escape.lua
+++ configs/nvim/.config/nvim/lua/pedro/plugins/better-escape.lua
@@ -1,6 +1,23 @@
 return {
   "max397574/better-escape.nvim",
   config = function()
-    require("better_escape").setup()
+    require("better_escape").setup {
+        timeout = vim.o.timeoutlen,
+        default_mappings = false,
+        mappings = {
+            i = {
+                j = {
+                    -- These can all also be functions
+                    k = "<Esc>",
+                    j = "<Esc>",
+                },
+                J = {
+                    -- These can all also be functions
+                    K = "<Esc>",
+                    J = "<Esc>",
+                },
+            },
+        },
+    }
   end,
 }
