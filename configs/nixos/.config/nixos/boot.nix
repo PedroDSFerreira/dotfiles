f6e02a34c1162dcca97bb27bb0bf85089fa7be5b
Pedro Ferreira <pedrodsf21@gmail.com>
2025-02-04 17:41:12 +0000

    Update boot.nix

--- configs/nixos/.config/nixos/boot.nix
+++ configs/nixos/.config/nixos/boot.nix
@@ -4,5 +4,20 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 2;
+
+  # --------Enable automatic login for the user.
+  # services.displayManager.autoLogin.enable = true;
+  # services.displayManager.autoLogin.user = "pedro";
+  #
+  # # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  # systemd.services."getty@tty1".enable = false;
+  # systemd.services."autovt@tty1".enable = false;
+  #
+  # systemd.sleep.extraConfig = ''
+  #   AllowSuspend=no
+  #   AllowHibernation=no
+  #   AllowHybridSleep=no
+  #   AllowSuspendThenHibernate=no
+  # '';
 }
 
