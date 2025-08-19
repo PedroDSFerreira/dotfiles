47d84d9f721ba20005ee5d9a479bac8f6e9e2b41
Pedro Ferreira <pedrodsf21@gmail.com>
2025-08-19 16:32:06 +0100

    Update nixos install script

--- scripts/nixos-setup.sh
+++ scripts/nixos-setup.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-# Exit immediately if a command exits with a non-zero status
 set -e
 
+echo "Setting up NixOS channels..."
+sudo nix-channel --add https://channels.nixos.org/nixos-25.05 nixos
+sudo nix-channel --add https://channels.nixos.org/nixos-unstable unstable
+sudo nix-channel --update
+
 echo "Installing stow..."
-nix-env -iA stow
+nix-env -iA nixos.stow
 
 echo "Creating symlinks..."
 stow -d configs -t $HOME \
@@ -33,4 +37,13 @@ echo "Removing default nixos folder..."
 sudo rm -rf /etc/nixos/
 
 echo "Building configurations..."
-sudo nixos-rebuild switch -I nixos-config=/home/pedro/.config/nixos/configuration.nix
+sudo nixos-rebuild switch -I nixos-config=$HOME/.config/nixos/configuration.nix
+
+echo "Installing yazi plugins..."
+sh $HOME/.config/yazi/install.sh
+
+echo "Installing zsh plugins..."
+source $HOME/.zshenv
+
+echo "Installing neovim plugins..."
+nvim +Lazy
